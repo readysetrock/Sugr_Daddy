94d80818cca70096434f476968346077f0d28492
Fu Wong <fcwong@yahoo.com>
2015-09-30 17:12:06 -0700

    include schema

--- SugarrrHack.sql
+++ SugarrrHack.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `sugarrrhack` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `SugarrrHack`;
 -- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
 --
 -- Host: 127.0.0.1    Database: SugarrrHack
@@ -55,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-30 16:50:47
+-- Dump completed on 2015-09-30 17:11:27
