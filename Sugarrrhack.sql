52a72e8a2b64ebfad148374f08eba8d085d8b611
Jared D <d0tizzl3@gmail.com>
2015-10-01 18:58:09 -0700

    updated sugarrhack.sql

--- Sugarrrhack.sql
+++ Sugarrrhack.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE  IF NOT EXISTS `sugarrrhack` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `SugarrrHack`;
+USE `sugarrrhack`;
 -- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
 --
--- Host: 127.0.0.1    Database: SugarrrHack
+-- Host: 127.0.0.1    Database: sugarrrhack
 -- ------------------------------------------------------
 -- Server version	5.5.42
 
@@ -16,6 +16,33 @@ USE `SugarrrHack`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `messages` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -35,9 +62,8 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `messages` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +72,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (3,'Turtle Tom','Tommy','tom@tom.yahoo','1111111111','$2a$12$lixVOY0uq8VNVRkUmIAWausBu7gEp5iTZUktyCorOJrF6Uw.hCzzm',NULL,'2015-09-30 18:17:41','2015-09-30 18:17:41',NULL,NULL),(4,'jared','jared','jared@mail.com',NULL,'$2a$12$A/wSZtfr/XWSSKJbL3fNBerLSGdsiLQINr03LDSnmdB27qC42w.uW',NULL,'2015-09-30 19:17:25','2015-09-30 19:17:25',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -59,4 +84,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-30 19:21:10
+-- Dump completed on 2015-10-01 18:55:40
